fadbb07ec6736b12e214fde839e10d633ec507ab
boazfr1 <boazfr1@gmail.com>
2024-07-10 11:52:26 +0300

    add cmake file

---
+++ build/CMakeFiles/TcpServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TcpServer.dir/http_tcpServer_linux.cpp.o"
+  "CMakeFiles/TcpServer.dir/http_tcpServer_linux.cpp.o.d"
+  "CMakeFiles/TcpServer.dir/server_linux.cpp.o"
+  "CMakeFiles/TcpServer.dir/server_linux.cpp.o.d"
+  "TcpServer"
+  "TcpServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TcpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
